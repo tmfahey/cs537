2fa608ccd2aca9136a6a7d8ec3d16bae9ab902a0
Taylor Fahey <fahey085@gmail.com>
2014-10-18 15:18:53 -0500

    done with malloc code

--- P3/P3A/mem.h
+++ P3/P3A/mem.h
@@ -1,11 +1,14 @@
-#ifndef __mem_h__
-#define __mem_h__
+#ifndef _MEM_H_
+#define _MEM_H_
 
-int Mem_Init(int sizeOfRegion);
-void *Mem_Alloc(int size);
-int Mem_Free(void *ptr);
+int Mem_Init(int size);
+
+void* Mem_Alloc(int size);
+
+int Mem_Free(void* ptr);
+
+int Mem_Available();
+
 void Mem_Dump();
 
-#endif // __mem_h__
-
-
+#endif // _MEM_H_
