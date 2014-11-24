ff00187d5ead464a93851aef0b8d5403d308361e
Taylor Fahey <fahey@galapagos-34.cs.wisc.edu>
2014-11-23 18:01:47 -0600

    testing locks

--- P4/P4B/xv6/user/threadtest.c
+++ P4/P4B/xv6/user/threadtest.c
@@ -20,17 +20,26 @@ int main(int argc, char *argv[]) {
       sleep(100);
       i++;
     }
+    while(x<99999999){
+    lock(&l);
+    x++;
+    unlock(&l);
+    }
   } else {
     int j = 0;
     // parent
-    while(j<2){
+    while(j<3){
       printf(1, "x = %d\n", x);
       sleep(100);
       j++;
     }
-    lock(&l);
     printf(1, "Parent here");
-    unlock(&l);
+    while(x<99999999){
+     lock(&l);
+      x++;
+      unlock(&l);
+    }
+    printf(1, "x: %d", x);
     j = join();
     printf(1, "Join: %d\n", j);
   }
